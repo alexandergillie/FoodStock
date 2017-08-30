a1f3bc9bb803220112939264875e4c9459d85af8
alexandergillie <alexander.gillie@gmail.com>
2017-08-29 20:02:08 -0600

    Tuesday Night

--- app/src/main/java/com/gillie/foodstock/MainActivity.java
+++ app/src/main/java/com/gillie/foodstock/MainActivity.java
@@ -3,11 +3,46 @@ package com.gillie.foodstock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.joanzapata.iconify.Iconify;
+import com.joanzapata.iconify.fonts.EntypoModule;
+import com.joanzapata.iconify.fonts.FontAwesomeModule;
+import com.joanzapata.iconify.fonts.IoniconsModule;
+import com.joanzapata.iconify.fonts.MaterialCommunityModule;
+import com.joanzapata.iconify.fonts.MaterialModule;
+import com.joanzapata.iconify.fonts.MeteoconsModule;
+import com.joanzapata.iconify.fonts.SimpleLineIconsModule;
+import com.joanzapata.iconify.fonts.TypiconsModule;
+import com.joanzapata.iconify.fonts.WeathericonsModule;
+
+import de.nitri.slidingtoggleswitch.SlidingToggleSwitchView;
+
+public class MainActivity extends AppCompatActivity implements SlidingToggleSwitchView.OnToggleListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Iconify
+                .with(new FontAwesomeModule())
+                .with(new EntypoModule())
+                .with(new TypiconsModule())
+                .with(new MaterialModule())
+                .with(new MaterialCommunityModule())
+                .with(new MeteoconsModule())
+                .with(new WeathericonsModule())
+                .with(new SimpleLineIconsModule())
+                .with(new IoniconsModule());
+
+
+
     }
+
+    @Override
+    public void onToggle(int result) {
+        if (result == SlidingToggleSwitchView.LEFT_SELECTED)
+        //...
+        else
+        //...
+
 }
